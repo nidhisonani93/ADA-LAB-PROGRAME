f0f22dc9b9628aa9d3c4f1c2470f8cd7d11b63ed
nidhisonani93 <nidhisonani79@gmail.com>
2025-07-27 10:32:17 +0530

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,38 @@
+def merge_sort(nums):
+    if len(nums) > 1:
+        mid = len(nums) // 2
+        left = nums[:mid]
+        right = nums[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+
+n = int(input("Enter a total number: "))
+nums = [0] * n
+for i in range(n):
+    val = int(input(f"Enter number {i + 1}: "))
+    nums[i] = val
+
+merge_sort(nums)
+print(nums)
